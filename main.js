a3ba41798d617647b974461ebece8a319aa14423
leeeeeeeminji <ldfu0269@naver.com>
2022-07-11 16:08:31 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get("/", (req, res) => {
+    res.send("hello wowrld");
+});
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+});
