d29c06512ef9db1d36001f19077abfdd47986f9b
leeeeeeeminji <ldfu0269@naver.com>
2022-07-13 19:00:39 +0900

    ...

--- main.js
+++ main.js
@@ -11,10 +11,6 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended : true}))
 
-app.get("/", (req, res) => {
-    res.send("hello wowrld");
-});
-
 //회원가입 하기
 app.post("/api/join", (req, res) => {
     const id = req.body.cusID
@@ -30,16 +26,53 @@ app.post("/api/join", (req, res) => {
     })
 })
 
+
 //로그인 하기
 app.post("/api/login", (req, res) => {
     const id = req.body.cusID
     const pw = req.body.cusPW
 
-    //회원가입 쿼리
-    const loginQuery = "SELECT * FROM customer WHERE cusID = ? and cusPassword = ?"
-    db.query(loginQuery, [id, pw], (err, result) => {
-        //login 되게 바꿔야됨.
-        console.log(result);
+    //입력된 id와 동일한 id 가 db에 있는지 확인
+    const idQuery = "SELECT * FROM customer WHERE cusID = ?"
+    db.query(idQuery, id, (err, rows) => {
+        if(rows.length) {
+            if (rows[0].cusID == id ) {
+                //id가 존재할 경우, 비밀번호 확인
+                const pwQuery = "SELECT * FROM customer WHERE cusPassword = ?"
+                db.query(pwQuery, pw, (err, rows) => {
+                    if (err){
+                        throw err
+                    }
+                    if(rows.length) {
+                        //로그인 성공
+                        res.json("드디어 로그인 성공")
+                    } else{
+                        //비밀번호 오류
+                        res.json("비밀번호가 틀립니다.")
+                    }
+                })
+            }
+        } else {
+            //아이디 없음
+            res.json("아이디가 존재하지 않습니다.")
+        }
+    })
+});
+
+
+//상품검색
+app.post("/api/search", (req,res) => {
+    const item = req.body.searchItem
+    const listQuery = `SELECT * FROM product WHERE productName LIKE '%${item}%'`
+    db.query(listQuery, (err, result) => {
+        res.send(result)
+    })
+})
+
+//상품목록
+app.get("/api/list", (req,res) => {
+    const listQuery = 'SELECT * FROM product'
+    db.query(listQuery, (err, result) => {
         res.send(result)
     })
 })
